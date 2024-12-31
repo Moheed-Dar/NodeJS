586bd17964d2bbf866a9f3b2de7936219a08a089
Moheed-Dar <moheeddar324@gmail.com>
2024-12-31 21:26:25 +0500

    getting input files from command

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const http = require("http");
-const data =require('./data')
-http.createServer((req, resp) => {
-    resp.writeHead(200, { "content-type": "application/json" });
-    resp.write(JSON.stringify(data));
-    resp.end();
-  })
-  .listen(5000);
+const fs =require ('fs')
+const input =process.argv;
 
-const modi =require('./modi')
-http.createServer((req,resp)=>{
-  resp.writeHead(200,{"content-type":"application/json"});
-  resp.write(JSON.stringify(modi));
-  resp.end();
-
-}).listen(5001);
+if(input[2]=='add')
+  {
+  fs.writeFileSync(input [3],input[4])
+}
+else if(input[2]=='remove'){
+  fs.unlinkSync(input[3])
+}
+else{
+  console.log("invalid input")
+}
--- modi.js
+++
@@ -1,6 +0,0 @@
-const modi =[
-    {school: "avicena science collage", dept:"computer"},
-    {school: "model science collage ", dept:"computer"},
-    {school: "boys high school", dept:"information"},
-]
-module.exports=modi;
