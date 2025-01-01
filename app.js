4b3742079a8e9cd9217b918897c06ed408fcb905
Moheed-Dar <moheeddar324@gmail.com>
2025-01-01 12:05:52 +0500

    this is read file text and utf-8 is used for removing buffer

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const fs =require ('fs')
 const path = require('path');
 const dirPath=path.join(__dirname,'crud')
 const filePath = `${dirPath}/apple.txt`
-fs.writeFileSync(filePath,'this is the simple text file')
-// 2nd method for creating the file
-// fs.writeFileSync(`${dirPath}/index.txt`,"this is index file text")
 
+
+fs.readFile(filePath,'utf-8',(err,item)=>{
+  console.log(item)
+})
