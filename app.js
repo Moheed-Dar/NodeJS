b0bfb6c22566db573b0fcc1dd14c09eab11770b8
Moheed-Dar <moheeddar324@gmail.com>
2025-01-01 11:48:47 +0500

    creating mpl+ files and read files

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const fs =require ('fs')
-const input =process.argv;
-
-if(input[2]=='add')
-  {
-  fs.writeFileSync(input [3],input[4])
+const path =require ('path');
+const dirPath = path.join(__dirname,'files');
+//it is used for creating multiples files
+for(i=0;i<5;i++){
+fs.writeFileSync(dirPath+"/sonu"+i+".txt","a simple text files")
+// fs.writeFileSync(dirPath+`/data${i}.txt`,"a simple text files")
 }
-else if(input[2]=='remove'){
-  fs.unlinkSync(input[3])
-}
-else{
-  console.log("invalid input")
-}
+//it is used for  read files 
+fs.readdir(dirPath,(err,item)=>{
+  item.forEach((atom)=>{
+    console.log(atom)
+  })
+})
