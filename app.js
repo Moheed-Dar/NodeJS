3b4bc236afb42db323eb8c82f22931202baf4639
Moheed-Dar <moheeddar324@gmail.com>
2025-01-01 16:43:07 +0500

    asyncroness 1

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const fs =require ('fs')
-const path = require('path');
-const dirPath=path.join(__dirname,'crud')
-const filePath = `${dirPath}/apple.txt`
+console.log("start exc...")
 
-fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
-  if (!err) console.log("file name is updated")
-})
+setTimeout(()=>{
+  console.log("logic exe..")
+},2000)
+
+console.log("complele exe...")
