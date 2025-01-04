dbdb0e2e825de7ed762c5d0f0fa6e6534beece0a
Moheed-Dar <moheeddar324@gmail.com>
2025-01-04 15:43:57 +0500

    render html and json data and also parameter sa values get ke ha input box ma

--- app.js
+++ app.js
@@ -2,13 +2,29 @@
  const express = require('express');
  const app = express();
  app.get('',(req,resp)=>{
-    console.log("data send by browser ",req.query.name)
-    resp.send("welcome , there this is  "+req.query.name);
+   
+    resp.send(`<h2> hello this is home page</h2><a href="/about">Go to about page</a>`);
  })
  app.get('/about',(req,resp)=>{
-    resp.send("welcome, this is about; page")
+    resp.send(`
+         <input type="text" placeholder="username"value=" ${req.query.name}" /><br>
+         <button>Click me </button><br>
+         <a href="/">Go To Home Page</a>
+
+        `)
  });
  app.get('/help',(req,resp)=>{
-    resp.send("welcome  , this is help; page")
- });
+    resp.send([
+        {
+        name:'moheed',
+        class:'bsit',
+        email:'moheeddar324@gmail.com'
+    },
+    {
+        name:'saad',
+        class:'bscs',
+        email:'saad324@gmail.com'
+    }
+]);
+});
  app.listen(5000)
