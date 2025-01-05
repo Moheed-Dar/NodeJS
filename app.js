3ae248f602347389a389806d18ac87a46b2ed1e5
Moheed-Dar <moheeddar324@gmail.com>
2025-01-05 13:08:12 +0500

    single route and groud route middle ware

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
 const reqFilter=require('./middle.js')
+const route = express.Router();
 // app.use(reqFilter);
+route.use(reqFilter);
 app.get("/", (req, resp) => {
   resp.send("<h1>welcome to home page</h1>");
 });
-app.get("/user", reqFilter, (req, resp) => {
+route.get("/user",  (req, resp) => {
   resp.send("<h2>welcome to user page</h2>");
 });
-app.get("/about", reqFilter, (req, resp) => {
-  resp.send("<h2>welcome to about page</h2>");
+ 
+route.get("/contact", (req, resp) => {
+  resp.send("<h2>welcome to contact page</h2>");
 });
+// single middleware app.get
+app.get("/detail", reqFilter, (req, resp) => {
+  resp.send("<h2>welcome to contact page</h2>");
+});
+//
+ app.use('/',route);
 app.listen(5000);
