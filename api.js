97220d4edb3bfb0130eeafe98840d35859af54f4
Moheed-Dar <moheeddar324@gmail.com>
2025-01-17 20:44:01 +0500

    delete api

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const dbConnect = require('./mongodb');
 const dbConnect1 = require ('./mongodb');
+const mongodb =  require('mongodb')
 const app = express();
 app.use(express.json());
 
@@ -24,5 +25,11 @@ app.put('/:name',async (req,resp)=>{
         {$set:req.body}
     )
     resp.send({result:"updated"})
+})
+
+app.delete('/:id',async(req,resp)=>{
+    let data = await dbConnect1();
+    let result = await  data.deleteOne({_id: new mongodb.ObjectId(req.params.id)})
+    resp.send(result)
 })
 app.listen(4000);
