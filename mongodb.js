292e9dc573753a89e5d154fa448119d61305f22b
Moheed-Dar <moheeddar324@gmail.com>
2025-01-17 16:37:28 +0500

    post API

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require("mongodb");
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
-async function dbConnect() {
+
+async function dbConnect() 
+{
   let result = await client.connect();
   let db = result.db("Shop");
   return db.collection("items");
 }
+
 module.exports = dbConnect;
