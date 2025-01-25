5e4d0a6edc230c578d258bd217049da978c02bcb
Moheed-Dar <moheeddar324@gmail.com>
2025-01-25 10:52:40 +0500

    search api

--- app.js
+++ app.js
@@ -3,37 +3,21 @@ require('./config');
 const Product = require('./product');
 
 const app = express();
-app.use(express.json())
-//post api mongoose
-app.post("/create",async (req,resp)=>{
-    let Data = new Product(req.body)
-    let result = await Data.save();
-    console.log(result)
-    resp.send(result);
-});
-//get api mongoose
-app.get ("/listen", async(req,resp)=>{
-    let Data = await Product.find();
-    resp.send(Data);
-})
-//delete api mongoose
-app.delete ("/delete/:_id", async(req,resp)=>{
-    let Data =await Product.deleteOne(req.params)
-    resp.send("done");
-    console.log(Data)
-})
-//update api mongoose
-app.put ("/update/:_id", async(req,resp)=>{
-    let Data =await Product.updateOne(
-        req.params,
-        {
-            $set:req.body
-        }
-    )
-    resp.send("done");
-    console.log(Data);
-})
 
+app.get ("/search/:key",async(req,resp)=>{
+    let data = await Product.find(
+        {
+            $or:[
+                {"Name":{$regex:req.params.key}},
+                {"name":{$regex:req.params.key}},
+                {"Category":{$regex:req.params.key}},
+                {"brand":{$regex:req.params.key}}
+            ]
+        }
+    );
+    console.log(req.params.key);
+    resp.send(data);
+})
 
 
 app.listen(5000);
--- product.js
+++ product.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
     Name:String,
+    name:String,
     Brand:String,
+    brand:String,
     Price:Number,
-    Category:String
+    price:Number,
+    Category:String,
+    category:String,
 });
 module.exports = mongoose.model('products',ProductSchema);  
